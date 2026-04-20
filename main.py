c431b8add0c108a583c2792037e237521e237498
imimim <imimim.email@gmail.com>
2026-04-20 17:13:17 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyannote.audio
+from pydub import AudioSegment
+
+audiofile_path = "/home/imimim/gits/transcriber/audio/Imim-Gorby.m4a"
+
+wav_path = "/home/imimim/gits/transcriber/audio/Imim-Gorby.wav"
+
+#convert to .wav
+audio = AudioSegment.from_file(audiofile_path)
+# Export the audio as a WAV file
+audio.export(wav_path, format="wav")
+
